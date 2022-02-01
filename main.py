99bb16d88881c65fa3c3f0016f6221ad8cbf7482
Samuel Lewis <slewis@atlassian.com>
2022-02-01 16:54:32 +1100

    Fix loading and reading of schems

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+# #!/usr/bin/python3
 
 from nbtschematic import SchematicFile
 import logging
@@ -9,6 +9,8 @@ parser.add_argument('inputFile', type=str, nargs=1, help='The input schematic fi
 parser.add_argument('outputFile', type=str, nargs=1, help='The output animation file')
 parser.add_argument('-d', '--debug', help="Enable debug mode", action='store_true')
 
+
+
 args = parser.parse_args()
 logLevel = logging.INFO
 
@@ -16,9 +18,16 @@ if args.debug:
   logLevel = logging.DEBUG
 
 logging.basicConfig(level=logLevel, format='[%(asctime)s][%(levelname)s] %(message)s')
+logging.debug("Log level: %s", logLevel)
 
 logging.debug("Input file: %s", args.inputFile[0])
 logging.debug("Output file: %s", args.outputFile[0])
 
-sf = SchematicFile.load(args.inputFile[0])
-print(sf.blocks)
+sf = SchematicFile.load(args.inputFile[0]).get("Schematic")
+
+logging.info("Loaded schematic file %s", args.inputFile[0])
+logging.info("Dimensions %dw x %dh x %dl", sf.get("Width"), sf.get("Height"), sf.get("Length"))
+
+logging.debug("Properties", sf.keys())
+for k in sf.keys():
+  logging.debug("%s: %s", k, sf.get(k))
